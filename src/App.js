bb7cb2fa4d4fd8fad9d26af17f73844ce602edaf
christiancajiao <christiancamilocajiaochacon@gmail.com>
2022-08-15 20:49:42 -0500

    select with framework options, and fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react"
+
 
 function App() {
+
+  const [framework, setFramework] = useState('')
+  const [listPost, setListPost] = useState([])
+
+  let request = `https://hn.algolia.com/api/v1/search_by_date?query=${framework}&page=0`
+
+  useEffect(() => {
+    fetch(request)
+    .then(response => response.json())
+    .then(data => {
+      setListPost(data.hits)
+      console.log(data.hits)
+    })
+  }, [framework])
+
+  function getValueFramework(frameworkSelected) {
+    setFramework(frameworkSelected.target.value)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select onClick={getValueFramework}>
+        <option value="React">React</option>
+        <option value="Vue">Vue</option>
+        <option value="Angular">Angular</option>
+      </select>
+      <div>
+        {listPost.map((e) => {
+          return(
+            <div className='card' >
+              <span>{e.created_at}</span>
+              <p>{e.author}</p>
+              <span>{e.story_title}</span>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
